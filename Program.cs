0bd8d56d4c606680eb02a3a2ba4a715ad9933b9f
Joao Victor Gregorio <joaogregoriodsilva@gmail.com>
2024-05-04 20:05:43 -0300

    Desafio criando um sistema e abstraindo um celular com POO concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.Blue;
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(
+    numero: "12345", modelo: "77889", imei: "56894", 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("");
+
+Console.ForegroundColor = ConsoleColor.Green;
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(
+    numero: "6789", modelo: "5544", imei: "8945", 128
+);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
